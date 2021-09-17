cbe2f0c2f35151c5d8eec28a804d2b7c855d5a8d
jugurj <juriyunna27@gmail.com>
2021-09-17 23:33:10 +0300

    Added advantages section Implemented new component for page content advantages list section and added 4 advantage items with corresponding texts and SVG icons.

--- src/components/PageContent.vue
+++ src/components/PageContent.vue
@@ -3,16 +3,21 @@
     <section class="page__main-slider">
       <MainSlider />
     </section>
+    <section class="page__advantages">
+      <Advantages />
+    </section>
   </main>
 </template>
 
 <script>
 import MainSlider from "@/components/MainSlider";
+import Advantages from "@/components/Advantages";
 
 export default {
   name: "PageContent",
   components: {
     MainSlider,
+    Advantages,
   },
 };
 </script>
