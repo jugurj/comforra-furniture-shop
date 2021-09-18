0c71c93825b90e45426c166ff45f64a7eae0e262
jugurj <juriyunna27@gmail.com>
2021-09-19 02:39:25 +0300

    ProductsItem component styles Implemented styles for ProductsItem component product cards. Added product images directory to the assets and used an image to display on the card component.

--- src/components/ProductsItem.vue
+++ src/components/ProductsItem.vue
@@ -4,8 +4,8 @@
       <div class="product-item__label product-item__label--sale">-30%</div>
     </div>
 
-    <a href="" class="product-item__image _img">
-      <img src="" alt="" />
+    <a href="" class="product-item__image _ibg">
+      <img src="@/assets/img/products/01.jpg" alt="Product image" />
     </a>
 
     <div class="product-item__body">
@@ -38,4 +38,90 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.product-item {
+  background-color: #f4f5f7;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+
+  &__labels {
+    position: absolute;
+    z-index: 2;
+    display: flex;
+    flex-direction: column;
+    right: 20px;
+    top: 20px;
+  }
+
+  &__label {
+    flex: 0 0 48px;
+    width: 48px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    font-weight: 500;
+
+    &:not(:last-child) {
+      margin-bottom: 10px;
+    }
+
+    &--sale {
+      background-color: #e97171;
+    }
+
+    &--new {
+      background-color: #2ec1ac;
+    }
+  }
+
+  &__image {
+    padding-bottom: 300px/285px * 100%;
+  }
+
+  &__body {
+    flex: 1 1 auto;
+    padding: 16px 16px 30px 16px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  &__content {
+    flex: 1 1 auto;
+  }
+
+  &__title {
+    font-size: 24px;
+    font-weight: 600;
+    line-height: 1.2;
+    margin-bottom: 8px;
+  }
+
+  &__text {
+    color: $grayColor;
+    line-height: 1.5;
+    margin-bottom: 8px;
+  }
+
+  &__prices {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    line-height: 1.5;
+  }
+
+  &__price {
+    font-size: 20px;
+    font-weight: 600;
+
+    &--old {
+      font-size: 16px;
+      font-weight: normal;
+      margin-left: 16px;
+      color: #b0b0b0;
+    }
+  }
+}
+</style>
