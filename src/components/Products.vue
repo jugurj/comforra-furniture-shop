e2cc8e26efd862185ccbec589f8ce6fb77c235da
jugurj <juriyunna27@gmail.com>
2021-09-19 02:21:03 +0300

    Products list layout Implemented general layout styles for products list and added global title rule

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -3,7 +3,7 @@
     <div class="products__container _container">
       <h2 class="products__title _title">Products</h2>
 
-      <div class="product__items">
+      <div class="products__items">
         <div class="products__item">
           <ProductsItem />
         </div>
@@ -23,4 +23,18 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.products {
+  padding-bottom: 40px;
+
+  &__title {
+    margin-bottom: 30px;
+  }
+
+  &__items {
+    display: grid;
+    gap: 30px;
+    grid-template-columns: repeat(4, 1fr);
+  }
+}
+</style>
