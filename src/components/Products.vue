cc32de4ee5db041c31cfeeca901c29b4c4987d7d
jugurj <juriyunna27@gmail.com>
2021-09-18 23:47:02 +0300

    Added ProductsItem component Added ProductsItem component which is responsible of the products card object. Implemented proper HTML structure for the component using BEM naming coventions. Included component in the Products (list) component.

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="products">
     <div class="products__container _container">
-      products
+      <h2 class="products__title _title">Products</h2>
+
+      <div class="product__items">
+        <div class="products__item">
+          <ProductsItem />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import ProductsItem from "@/components/ProductsItem.vue";
+
 export default {
   name: "Products",
+  components: {
+    ProductsItem,
+  },
 };
 </script>
 
---
+++ src/components/ProductsItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <article class="product-item">
+    <div class="product-item__labels">
+      <div class="product-item__label product-item__label--sale">-30%</div>
+    </div>
+
+    <a href="" class="product-item__image _img">
+      <img src="" alt="" />
+    </a>
+
+    <div class="product-item__body">
+      <div class="item-product__contet">
+        <h5 class="product-item__title">Syltherine</h5>
+        <div class="product-item__text">Stylish cafe chair</div>
+      </div>
+
+      <div class="product-item__prices">
+        <div class="product-item__price">Rp 2.500.000</div>
+        <div class="product-item__price product-item__price--old">
+          Rp 3.000.000
+        </div>
+      </div>
+
+      <div class="product-item__actions actions-product">
+        <div class="actions-product__body">
+          <a href="" class="actions-product__button btn">Add to cart</a>
+          <a href="" class="actions-product__link _icon-share">Share</a>
+          <a href="" class="actions-product__link _icon-favorite">Like</a>
+        </div>
+      </div>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "ProductsItem",
+};
+</script>
+
+<style></style>
