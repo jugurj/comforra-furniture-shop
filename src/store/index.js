05d82f451c81a5127feed27da131986a723543f4
jugurj <juriyunna27@gmail.com>
2021-09-27 23:46:42 +0300

    Added basic mutations to add, update and remove from cart store

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,33 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    cart: ["test-product"],
+    cart: [],
   },
-  mutations: {},
+  getters: {
+    getCart: (state) => {
+      return [...state.cart];
+    },
+  },
+  mutations: {
+    addCartItem(state, item) {
+      item.quantity = 1;
+      state.cart.push(item);
+    },
+
+    updateCartItem(state, updatedItem) {
+      state.cart = state.cart.map((cartItem) => {
+        if (cartItem.id == updatedItem.id) {
+          return updatedItem;
+        }
+
+        return cartItem;
+      });
+    },
+
+    removeCartItem(state, item) {
+      state.cart = state.cart.filter((cartItem) => {
+        return cartItem.id != item.id;
+      });
+    },
+  },
 });
