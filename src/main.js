7f5e82d1da385aa360e5caf18c582e8639870f21
jugurj <juriyunna27@gmail.com>
2021-09-27 23:39:56 +0300

    Added Vuex dependency and created store file

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import store from "./store";
+
 // import '@/assets/styles/styles.scss'
 
 const app = createApp(App);
+app.use(store);
 
 const clickOutsideDirective = {
   beforeMount: (el, binding) => {
