ae6197802daa8d930a64939f377a1f1df3ff3458
jugurj <juriyunna27@gmail.com>
2021-10-09 23:42:26 +0300

    Added template for the Gallery component and set of image assets

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,7 +1,117 @@
 <template>
   <div class="gallery">
-    <div class="gallery__container _container">
-      <h2 class="gallery__title _title">Gallery</h2>
+    <div class="gallery__container">
+      <div class="gallery__label">Share your setup with</div>
+      <h2 class="gallery__title _title">#ComforraFurniture</h2>
+
+      <div class="gallery__body">
+        <div class="gallery__items">
+          <div class="gallery__column">
+            <div class="gallery__row row-gallery row-gallery--left-top">
+              <a href="" class="row-gallery__item">
+                <img
+                  src="@/assets/img/furniture/09.jpg"
+                  alt="Furniture image"
+                />
+              </a>
+
+              <a href="" class="row-gallery__item">
+                <img
+                  src="@/assets/img/furniture/01.jpg"
+                  alt="Furniture image"
+                />
+              </a>
+
+              <a href="" class="row-gallery__item">
+                <img
+                  src="@/assets/img/furniture/02.jpg"
+                  alt="Furniture image"
+                />
+              </a>
+            </div>
+
+            <div class="gallery__row row-gallery row-gallery--left-bottom">
+              <a href="" class="row-gallery__item">
+                <img
+                  src="@/assets/img/furniture/04.jpg"
+                  alt="Furniture image"
+                />
+              </a>
+
+              <a href="" class="row-gallery__item">
+                <img
+                  src="@/assets/img/furniture/06.jpg"
+                  alt="Furniture image"
+                />
+              </a>
+
+              <a href="" class="row-gallery__item">
+                <img
+                  src="@/assets/img/furniture/07.jpg"
+                  alt="Furniture image"
+                />
+              </a>
+            </div>
+          </div>
+          <div class="gallery__column">
+            <div class="gallery__row row-gallery row-gallery--center">
+              <a href="" class="row-gallery__item">
+                <img
+                  src="@/assets/img/furniture/03.jpg"
+                  alt="Furniture image"
+                />
+              </a>
+            </div>
+          </div>
+          <div class="gallery__column">
+            <div class="gallery__row row-gallery row-gallery--right-top">
+              <a href="" class="row-gallery__item">
+                <img
+                  src="@/assets/img/furniture/04.jpg"
+                  alt="Furniture image"
+                />
+              </a>
+
+              <a href="" class="row-gallery__item">
+                <img
+                  src="@/assets/img/furniture/05.jpg"
+                  alt="Furniture image"
+                />
+              </a>
+
+              <a href="" class="row-gallery__item">
+                <img
+                  src="@/assets/img/furniture/01.jpg"
+                  alt="Furniture image"
+                />
+              </a>
+            </div>
+
+            <div class="gallery__row row-gallery row-gallery--right-bottom">
+              <a href="" class="row-gallery__item">
+                <img
+                  src="@/assets/img/furniture/08.jpg"
+                  alt="Furniture image"
+                />
+              </a>
+
+              <a href="" class="row-gallery__item">
+                <img
+                  src="@/assets/img/furniture/09.jpg"
+                  alt="Furniture image"
+                />
+              </a>
+
+              <a href="" class="row-gallery__item">
+                <img
+                  src="@/assets/img/furniture/06.jpg"
+                  alt="Furniture image"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
